d603dd873eaad2934fab2c40406e493690c9c7f8
Alejo Perea <mpereamejia@gmail.com>
2024-03-27 16:47:40 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas',[ComunaController::class, 'index']);
+
+Route::get('/prueba', [ComunaController::class, 'index']) -> name('comunas.create');
+
+
+
+
