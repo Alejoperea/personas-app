282ee5e27e03a268b783954233b0a94e236f27e3
Alejo Perea <mpereamejia@gmail.com>
2024-03-28 11:35:15 -0500

    Commune Delete

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/', function () {
 Route::get('/comunas',[ComunaController::class, 'index'])-> name('comunas.index'); //deberia ir index
 Route::post('/comunas',[ComunaController::class, 'store'])-> name('comunas.store');
 Route::get('/comunas/create',[ComunaController::class, 'create'])-> name('comunas.create');
+Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
 
 
 
