2dde280b029a896c199708ef3c9d7887515b970c
Alejo Perea <mpereamejia@gmail.com>
2024-03-28 12:58:25 -0500

    Municipio List

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,6 +28,10 @@ Route::get('/comunas/create',[ComunaController::class, 'create'])-> name('comuna
 Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
+
+//RUTAS MUNICIPIO
+
+Route::get('municipios',[MunicipioController::class,'index'])-> name('municipios.index');
 
 
 
